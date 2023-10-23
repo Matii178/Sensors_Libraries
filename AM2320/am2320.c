11e64de95e4be373e7405a3af8aa87790c500dde
Matii178 <mateusztomala178@gmail.com>
2023-10-23 22:25:43 +0200

    Wrong files has beed added.. correcting

--- AM2320/am2320.c
+++ AM2320/am2320.c
@@ -1,17 +1,20 @@
 #include "am2320.h"
 
 uint8_t AM2320_read(AM2320* const me, uint8_t startAdress, uint8_t bytes){
-
 	uint8_t funCodes[] = {0x03,startAdress,bytes};
 
-	HAL_I2C_Master_Transmit(me->I2Cinstance, AM2320_ADRESS, 0x00, 0, HAL_MAX_DELAY);
-	HAL_Delay(3);
-	if(HAL_I2C_Master_Transmit(me->I2Cinstance, AM2320_ADRESS, funCodes, 3, HAL_MAX_DELAY) != HAL_OK) {
+	HAL_Delay(100); // if it doesnt wait, then the error comes -> Sensor not being ready I guess ?
+
+	HAL_I2C_Master_Transmit(me->I2Cinstance, AM2320_ADRESS, 0x00, 1, 800);
+
+	HAL_Delay(1); //according to datasheet, there is a need to wait more than 800u and less than 3ms
+
+	if(HAL_I2C_Master_Transmit(me->I2Cinstance, AM2320_ADRESS, funCodes, 3, 2000)) {
 		return -1;
 	}
-	HAL_Delay(3);
-	if(HAL_I2C_Master_Receive(me->I2Cinstance, AM2320_ADRESS, me->data, bytes + 4, HAL_MAX_DELAY)) {
-		return -2;
+	HAL_Delay(2);
+	if(HAL_I2C_Master_Receive(me->I2Cinstance, AM2320_ADRESS, me->data, bytes + 4, 2000)) {
+		return -1;
 	}
 
 	return 0;
@@ -33,20 +36,36 @@ void AM2320_readTemperature(AM2320* const me) {
 
 void AM2320_readHumidity(AM2320* const me) {
 	uint16_t result;
+	float humidity;
 
 	if(AM2320_read(me, 0x00, 0x02)){
 		Error_Handler();
 	}
 	result = (me->data[2]) << 8 | (me->data[3]);
-	result /= 10;
-	me->temperature = (float) result;
+	humidity = result/10.0;
+	me->humidity = humidity;
 
 }
-uint32_t AM2320_introduceSelf(AM2320* const me){
-	return 1;
+
+void AM2320_readTempAndHum(AM2320* const me) {
+	uint16_t result;
+	float humidity;
+	float temperature;
+
+	if(AM2320_read(me, 0x00, 0x04)){
+		Error_Handler();
+	}
+	result = (me->data[2]) << 8 | (me->data[3]);
+	humidity = result/10.0;
+	result = (me->data[4]) << 8 | (me->data[5]);
+	temperature = result/10.0;
+	me->humidity = humidity;
+	me->temperature = temperature;
+
 }
 
-float AM2320_getTemperature(const AM2320* const me){
+
+float AM2320_getTemperature(AM2320* const me){
 	return me->temperature;
 }
 float AM2320_getHumidity(AM2320* const me){
@@ -55,10 +74,10 @@ float AM2320_getHumidity(AM2320* const me){
 
 }
 uint16_t AM2320_getModel(AM2320* const me){
-	return 1;
+	return me->model;
 }
 uint8_t AM2320_getVersion(AM2320* const me){
-	return 1;
+	return me->versionNum;
 
 }
 
@@ -67,9 +86,13 @@ void AM2320_Init(AM2320* const me,I2C_HandleTypeDef *hi2c){
 	me->I2Cinstance = hi2c;
 	me->humidity = 0;
 	me->temperature = 0;
-	me->model = 0;
-	me->versionNum = 0;
 	memset(me->data,0,8);
+//
+	AM2320_read(me, 0x08, 0x02);
+	me->model = (me->data[2] << 8) | (me->data[3]);
+	HAL_Delay(100);
+	AM2320_read(me, 0x0A, 0x01);
+	me->versionNum = me->data[2];
 }
 
 AM2320 * AM2320_Create(I2C_HandleTypeDef* hi2c){
--- AM2320/am2320.h
+++ AM2320/am2320.h
@@ -24,13 +24,14 @@ struct AM2320{
 
 void AM2320_readTemperature(AM2320* const me);
 void AM2320_readHumidity(AM2320* const me);
-uint32_t AM2320_introduceSelf(AM2320* const me);
+void AM2320_readTempAndHum(AM2320* const me);
 uint8_t AM2320_read(AM2320* const me, uint8_t startAdress, uint8_t bytes);
 
-float AM2320_getTemperature(const AM2320* const me);
+float AM2320_getTemperature(AM2320* const me);
 float AM2320_getHumidity(AM2320* const me);
 uint16_t AM2320_getModel(AM2320* const me);
 uint8_t AM2320_getVersion(AM2320* const me);
+
 
 void AM2320_Init(AM2320* const me, I2C_HandleTypeDef * I2Cinstance);
 AM2320 * AM2320_Create(I2C_HandleTypeDef* hi2c);
