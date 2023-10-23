891c8ed6d44ed360856b13b9be4dfbdec2f5c2a4
Matii178 <mateusztomala178@gmail.com>
2023-10-23 22:22:16 +0200

    changed AM2320 in a way it doesnt create an error

--- AM2320/am2320.c
+++ AM2320/am2320.c
@@ -4,16 +4,12 @@ uint8_t AM2320_read(AM2320* const me, uint8_t startAdress, uint8_t bytes){
 
 	uint8_t funCodes[] = {0x03,startAdress,bytes};
 
-	while(HAL_I2C_IsDeviceReady(me->I2Cinstance, AM2320_ADRESS,20 , HAL_MAX_DELAY)){} //wait till the time I2C is ready
-
 	HAL_I2C_Master_Transmit(me->I2Cinstance, AM2320_ADRESS, 0x00, 0, HAL_MAX_DELAY);
-	HAL_Delay(1); //according to datasheet, there is a need to wait more than 800u and less than 3ms
+	HAL_Delay(3);
 	if(HAL_I2C_Master_Transmit(me->I2Cinstance, AM2320_ADRESS, funCodes, 3, HAL_MAX_DELAY) != HAL_OK) {
 		return -1;
 	}
-
-	HAL_Delay(2);
-
+	HAL_Delay(3);
 	if(HAL_I2C_Master_Receive(me->I2Cinstance, AM2320_ADRESS, me->data, bytes + 4, HAL_MAX_DELAY)) {
 		return -2;
 	}
@@ -37,19 +33,20 @@ void AM2320_readTemperature(AM2320* const me) {
 
 void AM2320_readHumidity(AM2320* const me) {
 	uint16_t result;
-	float humidity;
 
 	if(AM2320_read(me, 0x00, 0x02)){
 		Error_Handler();
 	}
 	result = (me->data[2]) << 8 | (me->data[3]);
-	humidity = result/10.0;
-	me->humidity = humidity;
+	result /= 10;
+	me->temperature = (float) result;
 
 }
+uint32_t AM2320_introduceSelf(AM2320* const me){
+	return 1;
+}
 
-
-float AM2320_getTemperature(AM2320* const me){
+float AM2320_getTemperature(const AM2320* const me){
 	return me->temperature;
 }
 float AM2320_getHumidity(AM2320* const me){
@@ -58,10 +55,10 @@ float AM2320_getHumidity(AM2320* const me){
 
 }
 uint16_t AM2320_getModel(AM2320* const me){
-	return me->model;
+	return 1;
 }
 uint8_t AM2320_getVersion(AM2320* const me){
-	return me->versionNum;
+	return 1;
 
 }
 
@@ -70,13 +67,9 @@ void AM2320_Init(AM2320* const me,I2C_HandleTypeDef *hi2c){
 	me->I2Cinstance = hi2c;
 	me->humidity = 0;
 	me->temperature = 0;
+	me->model = 0;
+	me->versionNum = 0;
 	memset(me->data,0,8);
-
-	AM2320_read(me, 0x08, 0x02);
-	me->model = (me->data[2] << 8) | (me->data[3]);
-
-	AM2320_read(me, 0x0A, 0x01);
-	me->versionNum = me->data[2];
 }
 
 AM2320 * AM2320_Create(I2C_HandleTypeDef* hi2c){
