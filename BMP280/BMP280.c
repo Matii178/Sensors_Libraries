4202e08a875339f2948ee95115c9e2fa52683628
Matii178 <mateusztomala178@gmail.com>
2023-10-30 19:51:39 +0100

    Added initialization functions

--- BMP280/BMP280.c
+++ BMP280/BMP280.c
@@ -1,5 +1,8 @@
 #include "BMP280.h"
 
+/*
+ * Creation of an object
+ */
 BMP280* BMP280_Create(I2C_HandleTypeDef *i2c_handler, uint8_t SDO_state) {
 	BMP280 *newObject = malloc(sizeof(BMP280));
 	if(newObject != NULL) {
@@ -7,43 +10,87 @@ BMP280* BMP280_Create(I2C_HandleTypeDef *i2c_handler, uint8_t SDO_state) {
 	}
 	return newObject;
 
+/*
+ * Destruction of an object
+ */
 }
 void BMP280_Destroy( BMP280* const me) {
 	free(me);
 }
+
+/*
+ * First initialization of parameters
+ */
 void BMP280_Init(BMP280* const me,I2C_HandleTypeDef* i2c_handler, uint8_t SDO_state){
-	memset(me->buffer,0,3);
+	memset(me->buffer,0,20);
+	if(SDO_state){
+		me->slaveAddress = 0x77 << 1;
+	} else {
+		me->slaveAddress = 0x76 << 1;
+	}
 	me->i2c_handler = i2c_handler;
 	me->rawPressure = 0;
 	me->rawTemperature = 0;
 
+	BMP280_ReadRegisters(me, 0xD0, 1);
+	me->id = me->buffer[0];
+
 	me->configReg = BMP280_getConfigReg(me);
 	me->ctrlMeasReg = BMP280_getCtrlMeasReg(me);
+
+	BMP280_ReadRegisters(me, 0x88, 24);
+
+	for(int i = 0; i < 3; i ++) {
+		me->calibT[i] = me->buffer[2*i + 1] << 8 | me->buffer[2*i];
+	}
+	for(int i = 0; i < 9; i ++) {
+		me->calibP[i] = me->buffer[6 + 2*i + 1] << 8 | me->buffer[6 + 2*i];
+	}
 }
 
-void BMP280_SensorInitialize(BMP280 * const me);
+/*
+ * Initialization of sensor with given settings
+ */
+void BMP280_SensorInitialize(BMP280 * const me, uint8_t mode, uint16_t standbyTime, uint16_t osrs_t, uint16_t osrs_p) {
+
+	uint8_t settings[2] = {0};
+	settings[0] = (osrs_t << 5) | (osrs_p << 2) | mode;
+	settings[1] = (standbyTime << 5) | 0b00000; // filters to zero, maybe ill later change that
+
+	HAL_I2C_Mem_Write(me->i2c_handler, me->slaveAddress, 0xF4, 1, settings, 1, HAL_MAX_DELAY); //??? tutaj nie dziala nawet jak sie chce 2B wpisac
+	HAL_I2C_Mem_Write(me->i2c_handler, me->slaveAddress, 0xF5, 1, &settings[1], 1, HAL_MAX_DELAY);
+
+
+}
 void BMP280_ReadTemperature(BMP280 * const me) {
 
 	//w zaleznosci od parametrow wewnetrznych...
 	//BMP280_ReadRegisters(me, 0xFA, 3..2..1);
+
 
 }
 void BMP280_ReadPressure(BMP280 * const me);
 void BMP280_ReadTemperatureAndPressure( BMP280 * const me);
 
 void BMP280_ReadRegisters(BMP280* const me, uint8_t address, uint8_t bytes) {
-	HAL_I2C_Master_Transmit(me->i2c_handler, me->slaveAddress, (uint8_t *) address, 1, HAL_MAX_DELAY); //not sure
-	HAL_I2C_Master_Receive(me->i2c_handler, me->slaveAddress, me->buffer, bytes, HAL_MAX_DELAY); //bytes cannot be greater than 3;
-}
-void BMP280_WriteRegisters(BMP280 * const me, uint8_t address, uint8_t data); //pointer to table?
+	uint8_t ret[2];
+	ret[0] = HAL_I2C_Master_Transmit(me->i2c_handler, me->slaveAddress | 0, &address, 1, HAL_MAX_DELAY); //not sure
+	ret[1] = HAL_I2C_Master_Receive(me->i2c_handler, me->slaveAddress | 1, me->buffer, bytes, HAL_MAX_DELAY); //bytes cannot be greater than 24;
 
+	ret[0] = 0;
+}
+
+void BMP280_WriteRegisters(BMP280 * const me, uint8_t address, uint8_t data); //pointer to table?
 void BMP280_setConfigReg(BMP280 * const me);
 void BMP280_setCtrlMeasReg(BMP280 * const me);
-
 float BMP280_getTemperature(BMP280 * const me);
 float BMP280_getPressure(BMP280 * const me);
 uint8_t BMP280_getSensorID(BMP280 * const me);
-uint8_t BMP280_getConfigReg(BMP280 * const me);
-uint8_t BMP280_getCtrlMeasReg(BMP280 * const me);
+uint8_t BMP280_getConfigReg(BMP280 * const me) {
+	return 1;
+}
+uint8_t BMP280_getCtrlMeasReg(BMP280 * const me){
+	return 1;
+}
 
 void BMP280_ResetSensor(BMP280 * const me);
--- BMP280/BMP280.h
+++ BMP280/BMP280.h
@@ -6,19 +6,50 @@
 #include <string.h>
 #include <main.h>
 
+#define SDO_STATE_GND 0
+#define SDO_STATE_VDD 1
+
+//modes
+#define SLEEP_MODE 	0b00
+#define FORCED_MODE 0b01
+#define NORMAL_MODE 0b11
+
+//standby times
+#define STB_0_5 	0b000
+#define STB_0_62	0b001
+#define STB_125		0b010
+#define STB_250		0b011
+#define STB_500		0b100
+#define STB_1000	0b101
+#define STB_2000	0b110
+#define STB_4000	0b111
+
+//osrs_t & p
+#define OSRS_RESET 	0b000
+#define OSRS_X1 	0b001
+#define OSRS_X2 	0b010
+#define OSRS_X4 	0b011
+#define OSRS_X8 	0b100
+#define OSRS_X16 	0b111
+
+
+
 typedef struct BMP280 BMP280;
 
 struct BMP280{
 	//20 bytes for temperature and pressure ?
 	uint32_t rawTemperature;
 	uint32_t rawPressure;
-	uint8_t buffer[3];
+	uint8_t buffer[24];
 
 	I2C_HandleTypeDef* i2c_handler;
-	uint16_t slaveAddress;
+	uint8_t slaveAddress;
 	uint8_t configReg;
 	uint8_t ctrlMeasReg;
+	uint8_t id;
 
+	uint16_t calibT[3];
+	uint16_t calibP[9];
 
 };
 
@@ -26,7 +57,7 @@ BMP280* BMP280_Create(I2C_HandleTypeDef * i2c_handler, uint8_t SDO_state);
 void BMP280_Destroy(BMP280* const me);
 void BMP280_Init(BMP280* const me,I2C_HandleTypeDef* i2c_handler, uint8_t SDO_state);
 
-void BMP280_SensorInitialize(BMP280 * const me);
+void BMP280_SensorInitialize(BMP280 * const me, uint8_t mode, uint16_t standbyTime, uint16_t osrs_t, uint16_t osrs_p);
 void BMP280_ReadTemperature(BMP280 * const me);
 void BMP280_ReadPressure(BMP280 * const me);
 void BMP280_ReadTemperatureAndPressure(BMP280 * const me);
